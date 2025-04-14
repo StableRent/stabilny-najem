9995fd891124cd82d50d53efbd3da001da6c1368
off-origami <baskakovihor@gmail.com>
2025-04-14 06:42:57 -0700

    added astro-favicons for dynamic favicons generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { SITE } from './src/consts';
 import sitemap from '@astrojs/sitemap';
 import { shield } from '@kindspells/astro-shield';
 
+import favicons from 'astro-favicons';
+
 const rootDir = new URL('.', import.meta.url).pathname;
 const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
 
@@ -13,10 +15,7 @@ const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
 export default defineConfig({
   site: SITE.CANONICAL_URL,
   output: 'static',
-  integrations: [
-    sitemap(),
-    shield({
-      sri: { hashesModule: modulePath },
-    }),
-  ],
+  integrations: [sitemap(), shield({
+    sri: { hashesModule: modulePath },
+  }), favicons()],
 });
