2dbe47b38a34f9c70668304ce6793c0fd6376674
Daniel Adrian <truedaniyyel@gmail.com>
2026-03-27 10:50:54 -0700

    added observ to get rid of forced reflow and added better link for smaller video size

--- src/utils/initSwiper.ts
+++ src/utils/initSwiper.ts
@@ -5,6 +5,8 @@ import type { SwiperOptions } from "swiper/types";
 export function initSwiper(sliderName: string, swiperConfig: SwiperOptions) {
   return new Swiper(`.${sliderName}-swiper`, {
     modules: [Navigation, Pagination, Keyboard],
+    observer: true,
+    observeParents: true,
     speed: 700,
     loop: true,
     autoHeight: true,
